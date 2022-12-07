cb4ab8a039231321b3e26db4793ec531f5b0d4e6
Felix Ljungqvist <75997643+qvisten12@users.noreply.github.com>
2022-12-07 21:08:55 +0100

    Fix link

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -7,6 +7,7 @@ interface Props {
   primaryText: string
   secondaryText?: string
   description?: string
+  href?: string
   imageUrl: string
   alignment?: IAlignment
   useAsRef?: boolean
@@ -18,21 +19,25 @@ export enum IAlignment {
   center = 'items-center',
 }
 
-const Card: FC<Props> = ({ primaryText = '', secondaryText = '', description = '', imageUrl = '', alignment, useAsRef }) => {
+const Card: FC<Props> = ({ primaryText = '', secondaryText = '', description = '', imageUrl = '', alignment, useAsRef, href = '/' }) => {
   return (
-    <Link href={'/'} tabIndex={0} id={useAsRef ? 'work' : ''} className="flex justify-between flex-col lg:flex-row gap-5">
+    <div tabIndex={0} id={useAsRef ? 'work' : ''} className="flex justify-between flex-col lg:flex-row gap-5">
       <div className="order-2 lg:order-1 flex flex-col justify-center items-start gap-2 tracking-wider w-full md:w-1/2">
         <h4 className="opacity-50 text-xl font-sfBold ">{primaryText}</h4>
         <h3 className="text-4xl font-sfBold">{secondaryText}</h3>
         <p className="font-akuratLight text-xl">{description}</p>
-        <button className="py-3 px-10 mt-2 text-lg bg-black text-white font-sfBold rounded-md">Read More</button>
+        <Link href={href} className="py-3 px-10 mt-2 text-lg bg-black text-white font-sfBold rounded-md">
+          Read More
+        </Link>
       </div>
       <div className={`${styles.cardImage} order-1 lg:order-2 relative bg-sky-100`}>
         <div className="absolute top-20 w-full h-full">
-          <Image src={imageUrl} quality={80} fill={true} loading="lazy" alt={secondaryText} />
+          <Link href={href}>
+            <Image src={imageUrl} quality={80} fill={true} loading="lazy" alt={secondaryText} />
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
 
